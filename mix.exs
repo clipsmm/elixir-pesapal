31b2c5515c285dc8c6cfb9ceb60c48fb0552b99d
Mitamita Cliff Oscar <mitacliff@gmail.com>
2023-06-30 17:14:52 +0300

    [Fix] Elixir req bump down to 1.12

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Pesapal.MixProject do
     [
       app: :pesapal,
       version: "0.1.2",
-      elixir: "~> 1.14",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
